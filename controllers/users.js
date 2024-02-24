eec22574b589ddeeb6569411f912883c69607768
EliyaRabia <eliyarabia@gmail.com>
2024-02-24 12:40:05 +0200

    update server

---
+++ controllers/users.js
@@ -0,0 +1,98 @@
+const userService = require('../services/users');
+const jwt = require('jsonwebtoken');
+
+const createUser = async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const displayName = req.body.displayName;
+    const photo = req.body.photo;
+    user = await userService.getUserByUsername(username);
+    if (!user) {
+        res.status(200).json(await userService.createUser(username, password, displayName, photo));
+    }else{
+        res.status(404).send('Username already exists');
+    }
+}
+
+const login = async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    user = await userService.getUser(username, password);
+    if (user) {
+        res.status(200).json(user);
+        const token = jwt.sign({ username: user.username}, "key")
+        res.json(token);
+    }else{
+        res.status(404).send('Username or password is incorrect');
+    }
+}
+
+const getUser = async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  user = await userService.getUser(username,password);
+  if (user) {
+    res
+      .status(200)
+      .json(req.body.username, req.body.displayName, req.body.photo);
+  } else {
+    res.status(404).send("Username not found");
+  }
+};
+
+const getUserByUsername = async (req, res) => {
+  const username = req.body.username;
+  user = await userService.getUserByUsername(username);
+  if (user) {
+    res
+      .status(200)
+      .json(req.body.username, req.body.displayName, req.body.photo);
+  } else {
+    res.status(404).send("Username not found");
+  }
+};
+
+const getUserById = async (req, res) => {
+    const id = req.params.id;
+    user = await userService.getUserById(id);
+    if (user) {
+        res.status(200).json(user);
+    }else{
+        res.status(404).send('User not found');
+    }
+
+}
+
+const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const username = req.body.username;
+    const password = req.body.password;
+    const displayName = req.body.displayName;
+    const photo = req.body.photo;
+    user = await userService.updateUser(id, username, password, displayName, photo);
+    if (user) {
+        res.status(200).json(user);
+    }else{
+        res.status(404).send('User not found');
+    }
+}
+
+const deleteUser = async (req, res) => {
+    const id = req.params.id;
+    user = await userService.deleteUser(id);
+    if (user) {
+        res.status(200).json(user);
+    }else{
+        res.status(404).send('User not found');
+    }
+}
+
+module.exports = {
+  createUser,
+  login,
+  getUserByUsername,
+  getUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
---
+++ services/users.js
@@ -0,0 +1,67 @@
+const User = require('../models/users');
+const jwt = require('jsonwebtoken');
+
+const createUser = async (username, password, displayName, photo) => {
+  const user = new User({
+    username,
+    password,
+    displayName,
+    photo,
+  });
+  try {
+    return await user.save();
+  } catch (error) {
+    res.status(404).send(error);
+  }
+};
+
+const getUser = async (username, password) => {
+  try {
+    return await User.findOne({ username, password }).exec();
+  } catch (error) {
+    res.status(404).send(error);
+  }
+};
+
+const getUserByUsername = async (req, res) => {
+  const { username } = req.body.username;
+  try {
+    return await User.findOne({ username }).exec();
+  } catch (error) {
+    res.status(404).send(error);
+    return null;
+  }
+};
+
+const getUserById = async (id) => {
+  try {
+    return await User.findById(id);
+  } catch (error) {
+    res.status(404).send(error);
+    return null;
+  }
+};
+    
+const updateUser = async (id, username, password, displayName, photo) => {
+  const user = await getUserById(id);
+  if (!user)
+   return null;
+  user.username = username;
+  user.password = password;
+  user.displayName = displayName;
+  user.photo = photo;
+  await user.save();
+  return user;
+};
+
+const deleteUser = async (id) => {
+  const user = await getUserById(id);
+  if (!user) 
+    return null;
+  await user.deleteOne();
+  return user;
+};
+
+
+
+module.exports = { createUser, getUser, getUserByUsername, getUserById,updateUser,deleteUser };
