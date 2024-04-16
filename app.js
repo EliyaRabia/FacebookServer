0f9d30d8df523631cfb6325a2ef463c27713cbe8
orshmuel9 <orshmuel9@gmail.com>
2024-04-16 15:24:49 +0300

    try to send urls to bloom filter

--- app.js
+++ app.js
@@ -17,8 +17,47 @@ const postsRoute = require("./routes/posts");
 process.env.NODE_ENV = 'local';
 const customEnv = require('custom-env');
 customEnv.env(process.env.NODE_ENV, './config');
+const urls = process.env.CONNECTION_URL.split(',');
+console.log(urls);
+let index = 0;
+const net = require('net');
 
+// Create a new TCP client
+const client = new net.Socket();
+//client.setNoDelay(true);
+// Connect to the TCP server
+// client.connect(5555, '192.168.209.128', () => {
+//     console.log('Connected to TCP server');
 
+//     // Send a URL to the server
+//     client.write('2 http://example.com');
+// });
+client.connect(5555, '192.168.209.128', () => {
+  console.log('Connected to TCP server');
+
+  // Send each URL to the server
+  urls.forEach(url => {
+      console.log(`Sending URL to server: ${url}`);
+      client.write(`1 ${url}\n`);
+  });
+});
+// Handle data from the server
+client.on('data', (data) => {
+    console.log('Received from server: ' + data);
+
+    // Close the client after receiving data
+    client.destroy();
+});
+
+// Handle close event
+client.on('close', () => {
+    console.log('Connection closed');
+});
+
+// Handle error event
+client.on('error', (err) => {
+    console.error('Error: ', err);
+});
 //require("custom-env").env(process.env.NODE_ENV, "./config");
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
